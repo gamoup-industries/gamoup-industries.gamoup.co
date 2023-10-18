50fe28372a72f4b384dceddb6c0dba1295cec512
GamoupIndustries <72953048+gamoup-industries@users.noreply.github.com>
2023-10-18 10:40:32 -0400

    Modify core.js

--- js/core.js
+++ js/core.js
@@ -1,11 +1,21 @@
 class Game {
     /**
      * @param {object} config
-     * @param {number} [config.width=640]
-     * @param {number} [config.height=480]
-     * @param {HTMLCanvasElement} [config]
+     * @param {number} [config.width=undefined]
+     * @param {number} [config.height=undefined]
+     * @param {HTMLCanvasElement} [config.canvas=undefined]
+     * @param {string} [canvas.backgroundColor=undefined]
      */
     constructor(config) {
-        
+        if (config.canvas) {
+            this.canvas = config.canvas;
+            this.canvas.width = config.width || this.canvas.width;
+            this.canvas.height = config.height || this.canvas.height;
+        } else {
+            this.canvas = document.createElement("canvas");
+            this.canvas.width = config.width || 640;
+            this.canvas.height = config.height || 480;
+            document.body.appendChild();
+        }
     }
 }
