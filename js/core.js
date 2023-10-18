c58db05ba76a32d57c9b89341064706808808f20
GamoupIndustries <72953048+gamoup-industries@users.noreply.github.com>
2023-10-18 08:42:25 -0400

    Modify js files

--- js/core.js
+++ js/core.js
@@ -1,19 +1,9 @@
-/**
- * @param {object} config
- * @param {number} [config.width=640]
- * @param {number} [config.height=480]
- */
-var Game = function(config) {
-    alert(config.width || 640);
-    this.canvas = document.getElementById("game");
-
-    this.create = function() {
-
-    };
-    this.update = function() {
-
-    };
-    this.draw = function() {
+class Game {
+    /**
+     * @param {object} config
+     * @param {number} [width=640]
+     */
+    constructor() {
         
     }
 }
